533ebd96c1f8a7b5926f8d857b3acc11b91f25a5
AizenFrog <aleksandr.antipin.1999@mail.ru>
2021-12-16 01:57:47 +0300

    start lz77

---
+++ include/lz77.hpp
@@ -0,0 +1,8 @@
+#include <cstdint>
+
+namespace lz77 {
+
+std::size_t lz77_encode(const std::uint8_t* src, const std::size_t src_size, std::uint8_t* dst);
+std::size_t lz77_decode(const std::uint8_t* src, const std::size_t src_size, std::uint8_t* dst);
+
+}
---
+++ src/lz77_decode.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+
+namespace lz77 {
+
+std::size_t lz77_decode(const std::uint8_t* src, const std::size_t src_size, std::uint8_t* dst)
+{
+    return -1;
+}
+
+}
