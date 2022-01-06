ca2c5128b0f83bcfc358c288667a04cc12d9b2e2
AizenFrog <aleksandr.antipin.1999@mail.ru>
2022-01-06 14:25:52 +0300

    probable fix appveyor

--- tests/huffman_tests.cpp
+++ tests/huffman_tests.cpp
@@ -1,9 +1,16 @@
+#include <cstring>
+
 #include "gtest/gtest.h"
+#include "huffman.hpp"
 
-TEST(all, test1) {
-    EXPECT_EQ(5, 5);
+constexpr int buffer_size = 100;
+
+TEST(Huffman, encode_default_sequence)
+{
+    char word[] = { "beep boop beer!" };
+    char buffer[buffer_size];
+    std::memset(buffer, 0, buffer_size);
+    ASSERT_NO_THROW(huffman::huffman_encode((std::uint8_t*)word, sizeof(word), (std::uint8_t*)buffer));
 }
 
-TEST(all, test2) {
-    EXPECT_NE(4, 5);
-}
+
